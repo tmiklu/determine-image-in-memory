372c8c39e8ccdf392fb8c3ba0803eb7eb882cbb7
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-02-09 16:07:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import imghdr
 import json
 import string
 import os
+import io
 import uuid
 
 
@@ -11,36 +12,20 @@ BUCKET = os.environ['BUCKET']
 
 s3_client = boto3.client('s3')
 
-def generate_html():
-    content = '''
-        <html>
-            <body>
-                <h1 align="center">Nahraj uctenku</h1></br>
-                <form align="center" class="form" id="myForm">
-                    <input type="file" name="myHiddenField">
-                    <button type="submit">Nahraj</button>
-                    <script>
-    
-                    </script>
-            </body>
-        </html>
-    '''
-    return content
 
 def handler(event, context):
     
     uname = str(uuid.uuid4())
     
-    if event['httpMethod'] == 'POST' :
+    if event['httpMethod'] == 'POST':
 
         image = event['body']
-        image_array = bytearray(image, 'ascii')
-        image_format = base64.b64decode(image_array)
-        image_extension = imghdr.what('', image_format);
-        #print(image_extension)
+        image_decode = base64.b64decode(bytearray(image, 'ascii'))
+        image_extension = imghdr.what('', image_decode);
         
-        s3_client.put_object(Bucket=BUCKET, Key=uname + '.' + image_extension, Body=image_format)
-        
+    
+        s3_client.put_object(Bucket=BUCKET, Key=uname + '.' + image_extension, Body=image_decode)
+    
         return {
             "statusCode": 200,
             "headers": {
@@ -58,4 +43,19 @@ def handler(event, context):
                 "body": generate_html()
                 
             }
+
+def generate_html():
+    content = '''
+        <html>
+            <body>
+                <h1 align="center">Nahraj uctenku</h1></br>
+                <form align="center" class="form" id="myForm">
+                    <input type="file" name="myHiddenField">
+                    <button type="submit">Nahraj</button>
+                    <script>
     
+                    </script>
+            </body>
+        </html>
+    '''
+    return content
